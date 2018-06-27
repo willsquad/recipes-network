4d4784ec658dae78ea75b915267b93e5e982f18c
Ashil John <willsquad.aj@gmail.com>
2018-06-27 15:37:41 +0530

    create-recipe page in progress

--- files/js/main.js
+++ files/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-   lightbox.option({
+   
+    lightbox.option({
       'resizeDuration': 400,
       'wrapAround': true,
       'alwaysShowNavOnTouchDevices' : true
+    });
+
+    $('select').niceSelect();
+
+    $('.add_photo_div').on('click', function(){
+        alert('Upload Image');
     });
 });
